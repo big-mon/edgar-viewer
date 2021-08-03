e60f42d2c73c7f3bf4fed316a12c9a7038577915
BIG-MON <mrmon93@gmail.com>
2021-08-04 07:05:00 +0900

    feat: custom footer

--- src/components/organisms/footer.js
+++ src/components/organisms/footer.js
@@ -4,22 +4,37 @@ import styles from "../../styles/footer.module.scss";
 
 const { Footer } = Layout;
 
-export default function TopFooter() {
+export default function TopFooter({ children, home }) {
+  const name = "Alpha Gazer";
+
   return (
     <>
-      <Footer className={styles.footer}>
-        <Link href="/">
-          <a>Alpha Gazer</a>
-        </Link>{" "}
-        Created by{" "}
-        <a
-          href="//twitter.com/BIG_MON"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mon
-        </a>
-      </Footer>
+      {home ? (
+        <Footer className={styles.index}>
+          Created by{" "}
+          <a
+            href="//twitter.com/BIG_MON"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mon
+          </a>
+        </Footer>
+      ) : (
+        <Footer className={styles.footer}>
+          <Link href="/">
+            <a>{name}</a>
+          </Link>{" "}
+          Created by{" "}
+          <a
+            href="//twitter.com/BIG_MON"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mon
+          </a>
+        </Footer>
+      )}
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import TopHeader from "../components/organisms/header";
+import TopFooter from "../components/organisms/footer";
 import { Row, Col } from "antd";
 import Image from "next/image";
 import styles from "../styles/index.module.scss";
@@ -10,9 +11,11 @@ export default function Page() {
         <Row align="middle" className={styles.full_height}>
           <Col xs={{ span: 23, offset: 1 }} lg={{ span: 12, offset: 1 }}>
             <TopHeader home />
+            <TopFooter home />
           </Col>
         </Row>
       </div>
+
       <Image
         className={styles.cover_image}
         src="/bg.jpg"
