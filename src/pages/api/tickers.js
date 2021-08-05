e59ee82d95b03802990777ef7d910d7bb220eccd
BIG-MON <mrmon93@gmail.com>
2021-08-06 05:23:44 +0900

    feat: add user-agent

--- src/pages/api/tickers.js
+++ src/pages/api/tickers.js
@@ -1,8 +1,12 @@
 import fetch from "isomorphic-unfetch";
+import randomUseragent from "random-useragent";
 
 /** EDGARからティッカー情報を取得 */
 export default async function tickers(req, res) {
-  const fetched = await fetch(`https://www.sec.gov/files/company_tickers.json`);
+  const url = `https://www.sec.gov/files/company_tickers.json`;
+  const fetched = await fetch(url, {
+    headers: { "User-Agent": randomUseragent.getRandom() },
+  });
   const json = await fetched.json();
 
   res.status(200).json(json);
