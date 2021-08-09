3d941e2471519c37ef9191bf7f52bb551d32ad9f
BIG-MON <mrmon93@gmail.com>
2021-08-10 01:03:11 +0900

    fix: initial load param issue

--- src/pages/stock/[cik].js
+++ src/pages/stock/[cik].js
@@ -1,39 +1,51 @@
 import useSWR from "swr";
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import { Board } from "../../components/organisms/Board";
 import { Skeleton } from "antd";
 import { fetcher } from "../../utils/fetcher";
+
+export default function Page() {
+  return (
+    <>
+      <Content />
+    </>
+  );
+}
+
+/** コンテンツ */
+function Content() {
+  // CIKを整形
+  const router = useRouter();
+  const { cik } = router.query;
+
+  // データを取得
+  const { data, isLoading } = useStock(cik);
+  if (isLoading)
+    return <Skeleton active loading={true} paragraph={{ rows: 20 }} />;
+  return <Board data={data} />;
+}
 
 /** 0埋め、右から切り取り */
 function zeroPadding(num, length) {
   return (`0000000000` + num).slice(-length);
 }
 
-export default function Page() {
-  // CIKを整形
-  const { cik } = useRouter().query;
+/** 決算情報の取得 */
+function useStock(cik) {
   const padCik = zeroPadding(cik, 10);
 
   // APIからデータを取得
   const url = `/api/companyfacts?cik=${padCik}`;
-  const { data, error } = useSWR(url, fetcher, {
+  const { data, error } = useSWR(cik ? url : null, fetcher, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
     shouldRetryOnError: false,
     dedupingInterval: 5000,
   });
 
-  return (
-    <>
-      {data ? (
-        <>
-          <Board data={data} />
-        </>
-      ) : (
-        <>
-          <Skeleton active loading={!data} paragraph={{ rows: 20 }} />
-        </>
-      )}
-    </>
-  );
+  return {
+    data: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 }
--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,15 +1,9 @@
-export function fetcher(url) {
-  return fetch(url, {
+export const fetcher = (url) =>
+  fetch(url, {
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json;charset=UTF-8",
       "User-Agent": "estrilda@damonge.com",
       "Accept-Encoding": "gzip, deflate",
       Host: "damonge.com",
     },
-    mode: "cors",
-  }).then((response) => {
-    if (!response.ok) return response.text();
-    response.json();
-  });
-}
+  }).then((res) => res.json());
