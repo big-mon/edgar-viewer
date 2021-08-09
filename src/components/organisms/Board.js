ff9751131e58e598084204b3cacdd7f4a4c2a740
BIG-MON <mrmon93@gmail.com>
2021-08-10 07:21:28 +0900

    fix: null

--- src/components/organisms/Board.js
+++ src/components/organisms/Board.js
@@ -13,8 +13,8 @@ export class Board extends React.Component {
   }
 
   render() {
-    // 元データを整形
-    const facts = deconstructOriginalFacts(this.props.data);
+    const data = this.props.data ?? {};
+    const facts = deconstructOriginalFacts(data);
     const revenues = createRevenueData(facts);
     const cashflow = createCashFlowData(facts);
 
@@ -45,177 +45,178 @@ export class Board extends React.Component {
 const deconstructOriginalFacts = (data) => {
   if (data === undefined) return {};
 
-  const dei = data.facts.dei;
-  const gaap = data.facts["us-gaap"];
+  const dei = data.facts?.dei ?? {};
+  const gaap = data.facts["us-gaap"] ?? {};
 
   return {
     /** 会社名 */
-    CompanyName: data.entityName,
+    CompanyName: data.entityName ?? "Unknown",
 
     /** 株式 */
     Shares: {
       /** 発行済み株式数 */
-      Total: dei.EntityCommonStockSharesOutstanding,
+      Total: dei.EntityCommonStockSharesOutstanding ?? {},
       /** 浮動株式の時価総額 */
-      FloatValue: dei.EntityPublicFloat,
+      FloatValue: dei.EntityPublicFloat ?? {},
     },
 
     /** 売上高 */
     Revenue: {
       /** 純売上高(2018-) */
-      Revenue: gaap.RevenueFromContractWithCustomerExcludingAssessedTax,
-      Revenue2016: gaap.Revenues,
-      Revenue2015: gaap.SalesRevenueNet,
+      Revenue: gaap.RevenueFromContractWithCustomerExcludingAssessedTax ?? {},
+      Revenue2016: gaap.Revenues ?? {},
+      Revenue2015: gaap.SalesRevenueNet ?? {},
       /** 売上原価 */
-      COGS: gaap.CostOfGoodsAndServicesSold,
+      COGS: gaap.CostOfGoodsAndServicesSold ?? {},
       /** 粗利益 */
-      GrossProfit: gaap.GrossProfit,
+      GrossProfit: gaap.GrossProfit ?? {},
     },
 
     /** 営業経費・利益 */
     Operating: {
       /** 研究開発費 */
-      RaD: gaap.ResearchAndDevelopmentExpense,
+      RaD: gaap.ResearchAndDevelopmentExpense ?? {},
       /** 販売費及び一般管理費 */
-      SGA: gaap.SellingGeneralAndAdministrativeExpense,
+      SGA: gaap.SellingGeneralAndAdministrativeExpense ?? {},
       /** 総事業費 */
-      ExpensesTotal: gaap.OperatingExpenses,
+      ExpensesTotal: gaap.OperatingExpenses ?? {},
       /** 営業利益 */
-      Income: gaap.OperatingIncomeLoss,
+      Income: gaap.OperatingIncomeLoss ?? {},
       /** 営業外収入 */
-      IncomeNonOperating: gaap.NonoperatingIncomeExpense,
+      IncomeNonOperating: gaap.NonoperatingIncomeExpense ?? {},
     },
 
     /** 純利益 */
     NetIncome: {
       /** 税引前純利益 */
       Before:
-        gaap.IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest,
+        gaap.IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest ??
+        {},
       /** 純利益 */
-      Final: gaap.NetIncomeLoss,
+      Final: gaap.NetIncomeLoss ?? {},
     },
 
     /** EPS */
     EPS: {
       /** EPS(普通) */
-      basic: gaap.EarningsPerShareBasic,
+      basic: gaap.EarningsPerShareBasic ?? {},
       /** EPS(希薄化) */
-      diluted: gaap.EarningsPerShareDiluted,
+      diluted: gaap.EarningsPerShareDiluted ?? {},
     },
 
     /** 資産 */
     Assets: {
       /** 資産の合計 */
-      Total: gaap.Assets,
+      Total: gaap.Assets ?? {},
 
       /** 流動資産 */
       Current: {
         /** 現金及び現金同等物 */
-        Cash: gaap.CashAndCashEquivalentsAtCarryingValue,
+        Cash: gaap.CashAndCashEquivalentsAtCarryingValue ?? {},
         /** 有価証券(流動資産) */
-        Marketable: gaap.MarketableSecuritiesCurrent,
+        Marketable: gaap.MarketableSecuritiesCurrent ?? {},
         /** 売掛金 */
-        AccountsReceivable: gaap.AccountsReceivableNetCurrent,
+        AccountsReceivable: gaap.AccountsReceivableNetCurrent ?? {},
         /** 棚卸資産 */
-        Inventory: gaap.InventoryNet,
+        Inventory: gaap.InventoryNet ?? {},
         /** 非売掛金 */
-        NonTrade: gaap.NontradeReceivablesCurrent,
+        NonTrade: gaap.NontradeReceivablesCurrent ?? {},
         /** その他の流動資産 */
-        Other: gaap.OtherAssetsCurrent,
+        Other: gaap.OtherAssetsCurrent ?? {},
         /** 流動資産の合計 */
-        Total: gaap.AssetsCurrent,
+        Total: gaap.AssetsCurrent ?? {},
       },
 
       /** 固定資産 */
       NonCurrent: {
         /** 有価証券(固定資産) */
-        Marketable: gaap.MarketableSecuritiesNoncurrent,
+        Marketable: gaap.MarketableSecuritiesNoncurrent ?? {},
         /** 有形固定資産 */
-        Property: gaap.PropertyPlantAndEquipmentNet,
+        Property: gaap.PropertyPlantAndEquipmentNet ?? {},
         /** その他の資産 */
-        Other: gaap.OtherAssetsNoncurrent,
+        Other: gaap.OtherAssetsNoncurrent ?? {},
         /** 固定資産の合計 */
-        Total: gaap.AssetsNoncurrent,
+        Total: gaap.AssetsNoncurrent ?? {},
       },
     },
 
     /** 負債及び株主資本 */
     Liabilities: {
       /** 負債の合計 */
-      Liabilities: gaap.Liabilities,
+      Liabilities: gaap.Liabilities ?? {},
       /** 株主資本の合計 */
-      StockHolders: gaap.StockholdersEquity,
+      StockHolders: gaap.StockholdersEquity ?? {},
       /** 負債及び株主資本の合計 */
-      Total: gaap.LiabilitiesAndStockholdersEquity,
+      Total: gaap.LiabilitiesAndStockholdersEquity ?? {},
 
       /** 流動負債 */
       Current: {
         /** 買掛金 */
-        AccountsPayable: gaap.AccountsPayableCurrent,
+        AccountsPayable: gaap.AccountsPayableCurrent ?? {},
         /** その他の流動負債 */
-        Other: gaap.OtherLiabilitiesCurrent,
+        Other: gaap.OtherLiabilitiesCurrent ?? {},
         /** 前受収益 */
-        Contract: gaap.ContractWithCustomerLiabilityCurrent,
+        Contract: gaap.ContractWithCustomerLiabilityCurrent ?? {},
         /** コマーシャル・ペーパー */
-        CommercialPaper: gaap.CommercialPaper,
+        CommercialPaper: gaap.CommercialPaper ?? {},
         /** 短期債 */
-        LongTermDebt: gaap.LongTermDebtCurrent,
+        LongTermDebt: gaap.LongTermDebtCurrent ?? {},
         /** 流動資産の合計 */
-        Total: gaap.LiabilitiesCurrent,
+        Total: gaap.LiabilitiesCurrent ?? {},
       },
 
       /** 固定負債 */
       NonCurrent: {
         /** 長期債 */
-        LongTermDebt: gaap.LongTermDebtNoncurrent,
+        LongTermDebt: gaap.LongTermDebtNoncurrent ?? {},
         /** その他の固定負債 */
-        Other: gaap.OtherLiabilitiesNoncurrent,
+        Other: gaap.OtherLiabilitiesNoncurrent ?? {},
         /** 固定負債の合計 */
-        Total: gaap.LiabilitiesNoncurrent,
+        Total: gaap.LiabilitiesNoncurrent ?? {},
       },
 
       /** 契約義務と偶発債務 */
-      Contingencies: gaap.CommitmentsAndContingencies,
+      Contingencies: gaap.CommitmentsAndContingencies ?? {},
 
       /** 株主資本 */
       Equity: {
         /** 株式 */
-        Stock: gaap.CommonStocksIncludingAdditionalPaidInCapital,
+        Stock: gaap.CommonStocksIncludingAdditionalPaidInCapital ?? {},
         /** 内部留保 */
-        RetainedEarning: gaap.RetainedEarningsAccumulatedDeficit,
+        RetainedEarning: gaap.RetainedEarningsAccumulatedDeficit ?? {},
         /** その他の包括利益 */
-        Other: gaap.AccumulatedOtherComprehensiveIncomeLossNetOfTax,
+        Other: gaap.AccumulatedOtherComprehensiveIncomeLossNetOfTax ?? {},
         /** 株主資本の合計 */
-        Total: gaap.StockholdersEquity,
+        Total: gaap.StockholdersEquity ?? {},
       },
     },
 
     /** 株主還元 */
     Dividends: {
       /** 配当 */
-      Dividends: gaap.Dividends,
+      Dividends: gaap.Dividends ?? {},
       /** 自社株買い */
-      Repurchased: gaap.StockRepurchasedAndRetiredDuringPeriodValue,
+      Repurchased: gaap.StockRepurchasedAndRetiredDuringPeriodValue ?? {},
     },
 
     /** キャッシュフロー */
     CashFlow: {
       /** 営業キャッシュフロー */
-      Operating: gaap.NetCashProvidedByUsedInOperatingActivities,
+      Operating: gaap.NetCashProvidedByUsedInOperatingActivities ?? {},
       /** 投資キャッシュフロー */
-      Investing: gaap.NetCashProvidedByUsedInInvestingActivities,
+      Investing: gaap.NetCashProvidedByUsedInInvestingActivities ?? {},
       /** 財務キャッシュフロー */
-      Financing: gaap.NetCashProvidedByUsedInFinancingActivities,
+      Financing: gaap.NetCashProvidedByUsedInFinancingActivities ?? {},
       /** 配当キャッシュフロー支出 */
-      Dividends: gaap.PaymentsOfDividends,
+      Dividends: gaap.PaymentsOfDividends ?? {},
       /** 自社株買いキャッシュフロー支出 */
-      Repurchase: gaap.PaymentsForRepurchaseOfCommonStock,
+      Repurchase: gaap.PaymentsForRepurchaseOfCommonStock ?? {},
     },
 
     /** 減価償却 */
     PaymentsToAcquire: {
-      ProductiveAssets: gaap.PaymentsToAcquireProductiveAssets,
-      PropertyPlant: gaap.PaymentsToAcquirePropertyPlantAndEquipment,
+      ProductiveAssets: gaap.PaymentsToAcquireProductiveAssets ?? {},
+      PropertyPlant: gaap.PaymentsToAcquirePropertyPlantAndEquipment ?? {},
     },
   };
 };
@@ -291,14 +292,13 @@ const createCashFlowData = (data) => {
       ...d,
       "OCF Margin": Math.round((d.OCF / d.Revenue) * 1000) / 10,
     }));
-  console.log(result);
 
   return result;
 };
 
 /** 階層をシンプルに整理 */
 const extractDetailData = (data, label) => {
-  if (data === undefined) return [];
+  if (data === undefined || !Object.keys(data).length) return [];
 
   const detail = data.units;
   const units = Object.keys(detail)[0];
